14caea4da51fad954cc05127aef5f8b5665ee178
Anthony Garza <agarza1972@yahoo.com>
2014-06-26 11:25:57 -0500

    Created a route for my resume page at the path '/resume' that returns 'This is my resume'. Created a route for my portfolio page at the path '/portfolio' that returns 'This is my portfolio'.

--- app/routes.php
+++ app/routes.php
@@ -10,8 +10,24 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+// Route::get('/', function() {
+// 	return 'we are home';
+// });
 
-Route::get('/', function()
-{
-	return View::make('hello');
+// Route::get('/sayhello/{name}', function($name)
+// {
+// 	if ($name == "Anthony")
+//     {
+//         return Redirect::to('/');
+//     }
+//     else
+//     {
+//         return "Hello, $name!";
+//     }
+// });
+Route::get('/resume', function() {
+	return 'This is my resume';
+});
+Route::get('/portfolio', function() {
+	return 'This is my portfolio';
 });
