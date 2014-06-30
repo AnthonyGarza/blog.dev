4e4de9d9a93c04eb3db92b313a1833a4ce89393c
Anthony Garza <agarza1972@yahoo.com>
2014-06-30 09:33:49 -0500

    Routed the resources posts to the PostsController

--- app/routes.php
+++ app/routes.php
@@ -10,9 +10,9 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-Route::get('/', function() {
-	return View::make('temp.my-first-view');
-});
+// Route::get('/', function() {
+// 	return View::make('temp.my-first-view');
+// });
 
 // Route::get('/sayhello/{name}', function($name)
 // {
@@ -35,14 +35,26 @@ Route::get('/', function() {
 
 // Create a route for your resume page at the path '/resume' that returns 'This is my resume'.
 // Create a route for your portfolio page at the path '/portfolio' that returns 'This is my portfolio'.
-Route::get('/resume', function(){
-	//return 'This is my resume';
-    return View::make('resume');
-});
-Route::get('/portfolio', function(){
-	//return 'This is my portfolio';
-    return View::make('portfolio');
-});
+// Route::get('/resume', function(){
+// 	//return 'This is my resume';
+//     return View::make('pages.resume');
+// });
+
+// Route::get('/portfolio', function(){
+// 	//return 'This is my portfolio';
+//     return View::make('pages.portfolio');
+// });
+
+Route::get('/', 'HomeController@showWelcome');
+
+Route::get('resume', 'HomeController@showResume');
+
+Route::get('portfolio', 'HomeController@showPortfolio');
+
+Route::resource('posts', 'PostsController');
+
+
+
 
 
 
@@ -59,36 +71,41 @@ Route::get('/portfolio', function(){
 //     }
 // });
 
-Route::get('/sayhello/{name}', function($name)
-{
-    if ($name == "Chris")
-    {
-        return Redirect::to('/');
-    }
-    else
-    {
-        $data = array('name' => $name);
-        return View::make('temp.my-first-view')->with($data);
-    }
-});
-
-Route::get('/rolldice', function() {
-	return $rand = rand(1,6);
-});
-
-Route::get('/rolldice/{guess}', function($guess) {
-
-	if (!is_numeric($guess)) {
-		return Redirect::to('/rolldice');
-	}
-
-	$min = 1;
-	$max = 6;
-	$rand = rand($min,$max);
-	$data = array(
-		'rand' => $rand,
-		'guess' => $guess);
-	return View::make('temp.roll-dice')->with($data);
 
 
-});
+Route::get('/sayhello/{name}', 'HomeController@sayHello');
+
+
+// Route::get('/sayhello/{name}', function($name)
+// {
+//     if ($name == "Chris")
+//     {
+//         return Redirect::to('/');
+//     }
+//     else
+//     {
+//         $data = array('name' => $name);
+//         return View::make('temp.my-first-view')->with($data);
+//     }
+// });
+
+// Route::get('/rolldice', function() {
+// 	return $rand = rand(1,6);
+// });
+
+// Route::get('/rolldice/{guess}', function($guess) {
+
+// 	if (!is_numeric($guess)) {
+// 		return Redirect::to('/rolldice');
+// 	}
+
+// 	$min = 1;
+// 	$max = 6;
+// 	$rand = rand($min,$max);
+// 	$data = array(
+// 		'rand' => $rand,
+// 		'guess' => $guess);
+// 	return View::make('temp.roll-dice')->with($data);
+
+
+// });
