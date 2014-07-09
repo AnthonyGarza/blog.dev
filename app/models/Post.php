242b37f3063efe319d14f716637587f5933e2fbc
Anthony Garza <agarza1972@yahoo.com>
2014-07-09 09:05:20 -0500

    added ability to uploaded images to post and also use markdown and HTML purifier in the post body

--- app/models/Post.php
+++ app/models/Post.php
@@ -6,6 +6,8 @@ class Post extends BaseModel {
 	// The db table this model relates to
     protected $table = 'posts';
 
+    protected $imgDir = 'img-upload';
+
     // Validation rules for  our model properties
     public static $rules = [
     	'title' => 'required|max:100',
@@ -17,4 +19,20 @@ class Post extends BaseModel {
 	    return $this->belongsTo('User');
 	}
 
+    public function addUploadedImage($image) {
+        $systemPath = public_path() . '/' . $this->imgDir . '/';
+
+        $imageName = $this->id . '-' . $image->getClientOriginalName();
+
+        $image->move($systemPath, $imageName);
+
+        $this->img_path = '/' . $this->imgDir . '/' . $imageName;
+    }
+
+    public function renderBody() {
+        $dirtyHTML = Parsedown::instance()->parse($this->body);
+        $config = HTMLPurifier_Config::createDefault();
+        $purifier = new HTMLPurifier($config);
+        return $purifier->purify($dirtyHTML);
+    }
 }
